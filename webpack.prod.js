f9de927b97014f5c70de7c7dbc3cd24b43b4582f
sabhas <sabirbk06@gmail.com>
2021-07-12 18:56:53 +0500

    fix webpack contenthash substitution in webpack.prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    filename: 'main.[contentHash].js',
+    filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
 });
